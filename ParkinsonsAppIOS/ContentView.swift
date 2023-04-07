0695fa371932a30deaa968dbe59393bda93fffc1
Zaal, Arnav, Abhi <zbelihomji@gmail.com>
2023-04-07 18:12:42 -0400

    Initial Commit

---
+++ ParkinsonsAppIOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ParkinsonsAppIOS
+//
+//  Created by Zaal on 4/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ParkinsonsAppIOS/ParkinsonsAppIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkinsonsAppIOSApp.swift
+//  ParkinsonsAppIOS
+//
+//  Created by Zaal on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ParkinsonsAppIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
